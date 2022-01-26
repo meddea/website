2242664efdbf98155a54aac8220ccc10ea63f808
Alex Binetskyi <johndoe@example.com>
2022-01-26 13:40:17 +0200

    update

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,25 @@
+const Image = require("@11ty/eleventy-img");
 const elements = require("elements");
+
+async function picturePngWebp(src, alt, width, height) {
+  if(alt === undefined) {
+      // You bet we throw an error on missing alt (alt="" works okay)
+      throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
+  }
+  
+  let metadata = await Image(src, {
+      formats: ['png', 'webp'],
+      outputDir: './_site/img'
+  });
+
+  // TODO: figure out that loading="lazy" decoding="async"
+  // TODO: set image name like param
+  
+  return `<picture>
+              <source type="image/webp" srcset="${metadata.webp[0].url}">
+              <img src="${metadata.png[0].url}" width="${width}" height="${height}" alt="${alt}">
+          </picture>`;
+}
 
 module.exports = function (eleventyConfig) {
   const markdownIt = require("markdown-it");
@@ -30,6 +51,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addShortcode("end_container", elements.end_container);
   eleventyConfig.addAsyncShortcode("picture", elements.picture);
   eleventyConfig.addAsyncShortcode("pictureSvgPng", elements.pictureSvgPng);
+  eleventyConfig.addAsyncShortcode("picturePngWebp", picturePngWebp);
 
   // design-system
   eleventyConfig.addShortcode("assets_block", elements.designSystem.assets_block);
